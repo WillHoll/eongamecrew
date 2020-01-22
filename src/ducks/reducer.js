9a105b6f1e5828c19cec1d9372cb830b360f01c3
Willholl <willhollingsworth15@yahoo.com>
2020-01-22 16:55:06 -0700

    setup

---
+++ src/ducks/reducer.js
@@ -0,0 +1,17 @@
+const initialState = {
+
+}
+
+// ACTION CONSTANTS 
+
+
+
+// ACTION BUILDERS
+
+
+// REDUCER FUNCTION
+export default function reducer(state = initialState, action)  {
+  switch(aciton.type) {
+    default: return state;
+  };
+};
